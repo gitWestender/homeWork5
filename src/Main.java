bd977ded4bc138c3f0f7cf6c4fff3773af023132
Westender <javaxwestender@gmail.com>
2023-07-19 20:07:33 +0300

    Add: task3

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         task1();
         task2("Android", 2021);
+        task3(2100);
     }
 
     static void task1() {
@@ -40,6 +41,16 @@ public class Main {
                 break;
             default:
                 System.out.println("Под вашу операционную систему нечего скачивать");
+        }
+    }
+
+    static void task3(int year){
+        System.out.println("\nЗадание 3:");
+
+        if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0){
+            System.out.println(year + " год является високосным");
+        } else {
+            System.out.println(year + " год не является високосным");
         }
     }
 }
